f46c9f6d414125b78d0661cd2a5155ae7fb3cbfa
weberedo <weber.kauer@gmail.com>
2025-03-18 20:52:17 -0300

    Adiciona funcao para inserir amigos na lista

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigos = [];
+
+function adicionarAmigo() {
+  let input = document.getElementById("amigo");
+  let nome = input.value.trim();
+  if (nome === "") {
+    alert("Por favor, insira um nome válido.");
+    return;
+  }
+
+  amigos.push(nome);
+  console.log(amigos);
+
+  input.value = "";
+}
