b8426d7553581164cbe6793912954182d7303451
weberedo <weber.kauer@gmail.com>
2025-03-18 21:07:28 -0300

    Adicionar funcao de exibir lista de nomes e sorteio de amigos

--- app.js
+++ app.js
@@ -14,4 +14,30 @@ function adicionarAmigo() {
   console.log(amigos);
 
   input.value = "";
+  atualizarLista();
+}
+
+function atualizarLista() {
+  let lista = document.getElementById("listaAmigos");
+  lista.innerHTML = "";
+
+  for (let amigo of amigos) {
+    let li = document.createElement("li");
+    li.textContent = amigo;
+    lista.appendChild(li);
+  }
+}
+
+function sortearAmigo() {
+  if (amigos.length === 0) {
+    alert("A lista de amigos está vazia! Adicione pelo menos um nome.");
+    return;
+  }
+
+  let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+  let amigoSorteado = amigos[indiceAleatorio];
+
+  document.getElementById(
+    "resultado"
+  ).innerHTML = `🎉 Amigo sorteado: ${amigoSorteado}`;
 }
